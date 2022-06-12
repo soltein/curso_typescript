607b96da934308d736236dc87ade9c0f78ca91bc
Julio Cezar <juliocezar@sfox.com.br>
2022-06-11 22:36:32 -0300

    teste prettier

---
+++ src/teste_conf_prettier.ts
@@ -0,0 +1,13 @@
+//Código aleatório para validar se o prettier está ok.
+
+let arrayDeNumeros: Array<number>;
+
+arrayDeNumeros = [1, 2, 3, 4, 5];
+
+arrayDeNumeros.forEach((element) => {
+  console.log(element);
+});
+
+arrayDeNumeros = [1, 2, 3, 4];
+
+console.log(arrayDeNumeros);
