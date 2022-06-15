e7ee84b14e1ae866b45895857c49ee0cb22d9e4c
Julio Cezar <juliocezar@sfox.com.br>
2022-06-15 20:02:10 -0300

    Teste singleton e static

---
+++ src/test_singleton_factory.ts
@@ -0,0 +1,27 @@
+//Exemplo exclusivo para entender o static e padrão singleton em typescript.
+//O exemplo abaixo é meramente de estudo, não devendo ser replicado em produção, devido a fatores como os dados de conexão estarem fixados no código, etc.
+export class Database {
+  private static database: Database;
+  private host = 'localhost';
+  private user = 'root';
+  private password = '123456';
+
+  connect(): void {
+    console.log(`Conectado: ${this.host}, ${this.user}, ${this.password}`);
+  }
+
+  static getDatabase(): Database {
+    if (Database.database) return Database.database;
+
+    Database.database = new Database();
+    return Database.database;
+  }
+}
+
+const db1 = Database.getDatabase();
+db1.connect();
+
+const db2 = Database.getDatabase();
+db2.connect();
+
+console.log(db1 === db2);
