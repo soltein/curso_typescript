021158cba62b50b49db65949e8970d257138eadb
Julio Cezar <juliocezar@sfox.com.br>
2022-06-18 13:22:22 -0300

    Estudo Generics, constraints

--- src/test_generics.ts
+++ src/test_generics.ts
@@ -17,3 +17,22 @@ const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const arrayFilter = myFilter(array, (item) => item % 2 === 0);
 
 console.log(arrayFilter);
+
+// constraints generics
+
+type GetKeys = <O, K extends keyof O>(obj: O, key: K) => O[K];
+
+const getKeys: GetKeys = (obj, key) => obj[key];
+
+const animal = {
+  cor: 'Rosa',
+  vacinas: ['Vacina 1', 'Vacina 2'],
+  nome: 'Cachorro',
+  idade: 1,
+};
+
+const vacinas = getKeys(animal, 'vacinas');
+const cor = getKeys(animal, 'cor');
+
+console.table(vacinas);
+console.log(cor);
